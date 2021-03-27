91f4a89e42327f3018911d2fdff57e67ac88f6a9
Aj Steinbacher <adamjsteinbacher@gmail.com>
2021-03-27 15:15:05 -0500

    app.js is now linked to the database in relationship to the posts, the seeded data is showing up

--- gram-clone/src/App.js
+++ gram-clone/src/App.js
@@ -1,30 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Posts from "./Components/Posts";
+import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "AjaxCodes",
-      caption: "Would you look at that Cat! ",
-      imageUrl:
-        "https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg ",
-    },
-    {
-      username: "AjaxCodes",
-      caption: "Would you look at that Cat! ",
-      imageUrl:
-        "https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg ",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+    // line 25 section will function like a for loop everytime a new post is made,
+    // to updated the database with the new posts
+  }, []);
 
   return (
     <div className="app">
       <div className="app_header">
         <Header />
       </div>
-      
+
       {posts.map((posts) => (
         <Posts
           username={posts.username}
@@ -32,7 +28,6 @@ function App() {
           imageUrl={posts.imageUrl}
         />
       ))}
-      
     </div>
   );
 }
