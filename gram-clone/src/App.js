15e45b23d51458d87ccca265207d949e553f9edc
Aj Steinbacher <adamjsteinbacher@gmail.com>
2021-03-29 16:40:00 -0500

    modal moved from app.js to header.js, button to open modal added, header also adjusted to have 50px margin-bottom from posts

--- gram-clone/src/App.js
+++ gram-clone/src/App.js
@@ -3,36 +3,14 @@ import "./App.css";
 import Header from "./Components/Header";
 import Post from "./Components/Post";
 import { db } from "./firebase";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
 
-function getModalStyle() {
-  const top = 50;
-  const left = 50;
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+
 
 function App() {
-  const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
-
+  
   const [posts, setPosts] = useState([]);
-  const [open, setOpen] = useState(false);
+  
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -47,11 +25,7 @@ function App() {
 
   return (
     <div className="app">
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <div style={modalStyle} className={classes.paper}>
-          <h2>Sign in modal </h2>
-        </div>
-      </Modal>
+     
 
       <div className="app_header">
         <Header />
--- gram-clone/src/Components/Header.js
+++ gram-clone/src/Components/Header.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState }from 'react';
 import "./Header.css";
-import gramCloneLogo from "../Images/gramCloneLogo.png";
+import gramClone from "../Images/gramClone.png";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Button from '@material-ui/core/Button';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: "absolute",
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 function Header() {
+    const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = useState(false);
 
-    
+  const signUp = (event) => {
+
+  }
+
+
+
     return (
         <div className="header">
+             <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>Sign in modal </h2>
+        </div>
+      </Modal>
              <img className="header_image" 
-             src={gramCloneLogo} alt="logo" />
+             src={gramClone} alt="logo" />
+
+             <Button onClick={() => setOpen(true)}>Sign Up</Button>
         </div>
     )
 }
