80c5858c28f16d2b4678fc3838e5b706a5a96cae
Aj Steinbacher <adamjsteinbacher@gmail.com>
2021-03-29 17:13:01 -0500

    modal created, concepting styling of modal, needs css work

--- gram-clone/src/Components/Header.js
+++ gram-clone/src/Components/Header.js
@@ -1,55 +1,81 @@
-import React, { useState }from 'react';
+import React, { useState } from "react";
 import "./Header.css";
 import gramClone from "../Images/gramClone.png";
+import gramCloneLogo from "../Images/gramCloneLogo.png";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
-import Button from '@material-ui/core/Button';
+import { Button, Input } from "@material-ui/core";
 
 function getModalStyle() {
-    const top = 50;
-    const left = 50;
-  
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    };
-  }
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      position: "absolute",
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }));
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Header() {
-    const classes = useStyles();
+  const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = useState(false);
 
-  const signUp = (event) => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  }
+  const signUp = (event) => {};
 
-
-
-    return (
-        <div className="header">
-             <Modal open={open} onClose={() => setOpen(false)}>
+  return (
+    <div className="header">
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <h2>Sign in modal </h2>
+            <form>
+            <center>
+            <img src={gramCloneLogo} alt="gram clone logo" />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          
+
+            </form>
+         
         </div>
       </Modal>
-             <img className="header_image" 
-             src={gramClone} alt="logo" />
+      <img className="header_image" src={gramClone} alt="logo" />
 
-             <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-    )
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
